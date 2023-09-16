1983dda5bed08c4f9eefea6b29bc25a13f571788
avinash <avinash.kumar70008@gmail.com>
2023-09-16 10:25:25 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useState } from 'react';
 import BottomSheet from './components/HalfBottom/BottomSheet';
 import './App.css';
 import FullBottomSheet from './components/FullBottom/FullBottomSheet';
+
 
 function App() {
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
@@ -10,50 +10,31 @@ function App() {
 
   const openBottomSheet = () => {
     setIsBottomSheetOpen(true);
-    setIsBottomSheetFullOpen(false); // Close the full-bottom sheet
   };
-
   const openBottomSheetFullOpen = () => {
-    setIsBottomSheetOpen(false); // Close the half-bottom sheet
     setIsBottomSheetFullOpen(true);
   };
-
   const closeBottomSheet = () => {
     setIsBottomSheetOpen(false);
   };
-
   const closeBottomSheetFull = () => {
     setIsBottomSheetFullOpen(false);
   };
-
-  // Define springs for animations
-  const bottomSheetSpring = useSpring({
-    height: isBottomSheetOpen ? '100%' : '0%',
-  });
-
-  const fullBottomSheetSpring = useSpring({
-    height: isBottomSheetFullOpen ? '100%' : '0%',
-  });
 
   return (
     <div className="App">
       <h1>Bottom Sheet App</h1>
       <div className="flex">
-        <button onClick={openBottomSheet} className="open-button">
-          Half-open Bottom Sheet
-        </button>
-        <button onClick={openBottomSheetFullOpen} className="open-button">
-          Fully open Bottom Sheet
-        </button>
+      <button onClick={openBottomSheet} className="open-button">
+      half-open Bottom Sheet
+      </button>
+      <button onClick={openBottomSheetFullOpen} className="open-button">
+      fully open Bottom Sheet
+      </button>
       </div>
-      <animated.div className="Edge" style={bottomSheetSpring}>
-        <BottomSheet isOpen={isBottomSheetOpen} onClose={closeBottomSheet} />
-      </animated.div>
-      <animated.div style={fullBottomSheetSpring}>
-        <FullBottomSheet isOpen={isBottomSheetFullOpen} onClose={closeBottomSheetFull} />
-      </animated.div>
+      <BottomSheet className="Edge" isOpen={isBottomSheetOpen} onClose={closeBottomSheet} />
+      <FullBottomSheet isOpen={isBottomSheetFullOpen} onClose={closeBottomSheetFull}/>
     </div>
   );
 }
-
-export default App;
+export default App
